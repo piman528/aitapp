14676301ed32e21243f014da719914131a473bff
piman528 <youhei0528.0722@gmail.com>
2024-01-23 12:31:50 +0900

    update

--- lib/screens/webview.dart
+++ lib/screens/webview.dart
@@ -1,8 +1,9 @@
 import 'package:aitapp/infrastructure/access_lcan.dart';
 import 'package:aitapp/provider/id_password_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewScreen extends StatefulHookConsumerWidget {
   const WebViewScreen({super.key});
@@ -12,69 +13,75 @@ class WebViewScreen extends StatefulHookConsumerWidget {
 }
 
 class _WebViewScreenState extends ConsumerState<WebViewScreen> {
-  late final WebViewController controller;
-  late final WebViewCookieManager cookieManager;
-
-  /// 初期状態を設定
-  @override
-  void initState() {
-    super.initState();
-    controller = WebViewController();
-    cookieManager = WebViewCookieManager();
-    getLoginCookie();
-  }
-
-  Future<void> getLoginCookie() async {
-    final cookies = await getCookie();
-    final identity = ref.read(idPasswordProvider);
-    await loginLcam(
-      identity[0],
-      identity[1],
-      cookies[0],
-      cookies[1],
-    );
-    print(cookies[0].split('=')[1]);
-    print(cookies[1].split('=')[1]);
-
-    cookieManager.setCookie(
-      WebViewCookie(
-        name: 'JSESSIONID',
-        value: cookies[0].split('=')[1],
-        domain:
-            'https://lcam.aitech.ac.jp/portalv2/smartphone/smartPhoneHome/nextPage/entryRegist/',
-      ),
-    );
-
-    cookieManager.setCookie(
-      WebViewCookie(
-        name: 'LiveApps-Cookie',
-        value: cookies[1].split('=')[1],
-        domain:
-            'https://lcam.aitech.ac.jp/portalv2/smartphone/smartPhoneHome/nextPage/entryRegist/',
-      ),
-    );
-    cookieManager.setCookie(
-      WebViewCookie(
-        name: 'L-CamApp',
-        value: 'Y',
-        domain:
-            'https://lcam.aitech.ac.jp/portalv2/smartphone/smartPhoneHome/nextPage/entryRegist/',
-      ),
-    );
-    controller.loadRequest(
-      Uri.parse(
-        'https://lcam.aitech.ac.jp/portalv2/smartphone/smartPhoneHome/nextPage/entryRegist/',
-      ),
-    );
-  }
+  late final InAppWebViewController webViewController;
+  late final CookieManager cookieManager;
+  final expiresDate =
+      DateTime.now().add(const Duration(days: 3)).millisecondsSinceEpoch;
+  final url = WebUri(
+    'https://lcam.aitech.ac.jp/portalv2/smartphone/smartPhoneHome/nextPage/entryRegist/',
+  );
 
   @override
   Widget build(BuildContext context) {
+    final load = useState(false);
+    Future<void> getLoginCookie() async {
+      final cookies = await getCookie();
+      final identity = ref.read(idPasswordProvider);
+      await loginLcam(
+        identity[0],
+        identity[1],
+        cookies[0],
+        cookies[1],
+      );
+      print(cookies[0].split('=')[1]);
+      print(cookies[1].split('=')[1]);
+      // getEntryRegist(cookies[0], cookies[1]);
+
+      await cookieManager.setCookie(
+        url: url,
+        name: 'JSESSIONID',
+        value: cookies[0].split('=')[1],
+        expiresDate: expiresDate,
+        isSecure: true,
+      );
+      await cookieManager.setCookie(
+        url: url,
+        name: 'LiveApps-Cookie',
+        value: cookies[1].split('=')[1],
+        expiresDate: expiresDate,
+        isSecure: true,
+      );
+      await cookieManager.setCookie(
+        url: url,
+        name: 'L-CamApp',
+        value: 'Y',
+        expiresDate: expiresDate,
+        isSecure: true,
+      );
+      load.value = true;
+    }
+
+    useEffect(
+      () {
+        cookieManager = CookieManager.instance();
+        getLoginCookie();
+        return null;
+      },
+      [],
+    );
+
     return Scaffold(
       appBar: AppBar(),
-      body: WebViewWidget(
-        controller: controller,
-      ),
+      body: load.value
+          ? InAppWebView(
+              initialUrlRequest: URLRequest(
+                url: url,
+              ),
+              onWebViewCreated: (controller) {
+                webViewController = controller;
+              },
+            )
+          : const SizedBox(),
     );
   }
 }
