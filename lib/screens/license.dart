885548eb0151a73d3a38354d9d937e2882adae3a
piman528 <youhei0528.0722@gmail.com>
2024-02-01 23:48:39 +0900

    パッケージを最新に

--- lib/screens/license.dart
+++ lib/screens/license.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:package_info/package_info.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class LicenseScreen extends HookWidget {
   const LicenseScreen({super.key});
